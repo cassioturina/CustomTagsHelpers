bd949f4a726c796496661642532103a220f9ff98
Cassio Turina <cassioturina.net@gmail.com>
2020-12-28 23:12:40 -0300

    Add project files.

---
+++ CustomTagsHelpers/Extensions/SimNaoTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+
+namespace CustomTagsHelpers.Extensions
+{
+    [HtmlTargetElement("span", Attributes = "sim-nao")]
+    public class SimNaoTagHelper : TagHelper
+    {
+        [HtmlAttributeName("sim-nao")]
+        public bool Value { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            if (output == null)
+                throw new ArgumentNullException(nameof(output));
+
+            var badgeType = Value ? "badge-success" : "badge-danger";
+
+            var texto = Value ? "SIM" : "NÃO";
+
+            output.Attributes.Add(new TagHelperAttribute("class", $"badge badge-pill p-2 rounded  {badgeType}"));
+            output.Content.Append(texto);
+        }
+
+
+    }
+
+}
---
+++ CustomTagsHelpers/Extensions/SomenteAdminTagHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+
+namespace CustomTagsHelpers.Extensions
+{
+    [HtmlTargetElement("a", Attributes = "somente-admin")]
+    public class SomenteAdminTagHelper : TagHelper
+    {
+        [HtmlAttributeName("somente-admin")]
+        public bool EhAdmin { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            if (output == null)
+                throw new ArgumentNullException(nameof(output));
+
+
+            if (EhAdmin) return;
+
+            output.Attributes.RemoveAll("href");
+            output.Attributes.Add(new TagHelperAttribute("style", "cursor: not-allowed"));
+            output.Attributes.Add(new TagHelperAttribute("title", "Acesso Somente para Administradores"));
+        }
+    }
+
+}
